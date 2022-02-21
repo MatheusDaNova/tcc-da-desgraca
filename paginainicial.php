fdf156dc35c4ff04b1130ab9b2fec708d7cf4b86
Matheus Da Nova <55743810+MatheusDaNova@users.noreply.github.com>
2022-02-21 19:44:43 -0300

    Update paginainicial.php

--- paginainicial.php
+++ paginainicial.php
@@ -5,55 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>i aluno</title>
-    <style>
-        body{
-            font-family: arial, helvetica, sans-serif;
-            background: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
-        }
-       div{
-           background-color: rgb(0, 0, 0, 0,6);
-           position: absolute;
-           top: 50%;
-           left: 50%;
-           transform: translate(-50,-50);
-           padding: 80px;
-           border-radius: 15px;
-           color: #fff;
-       }
-       input{
-           padding: 15%;
-           border: none;
-           outline: none;
-           font-size: 15px;
-
-       }
-       .inputsubmit{
-           background-color: dodgerblue;
-           border: none;
-           padding: 15%;
-           width: 100%;
-           border-radius: 10px;
-           color: white;
-           font-size: 15px;
-       }
-       .inputsubmit:hover{
-           background-color: deepskyblue;
-           cursor: pointer;
-       }
-        
-    </style>
+    
+    <link rel="stylesheet" type="text/css" href="css.css" media="screen" />
 </head>
 <body>
-    <a href="index.php">voltar</a>
-    <div>
-        <h1></h1>
-        <input type="text" placeholder="Nome">
-        <br><br>
-        <input type="password" placeholder=""senha"">
-        <br><br>
-        <input type="submit" name="submit" value="enviar">
-        <br><br>
-        <input class="inputsubmit" type="submit" name="submit" value="enviar">
-     </div>
+   
+    <div class="box-search">
+        <input type="search" class="form-control w-25" placeholder="Pesquisar" id="pesquisar">
+        <button onclick="searchData()" class="btn">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+            </svg>
+        </button>
+    </div>
+    <br><br>
+    
+    <a class="btn2" href="index.php">log out</a>
+    <a class="btn" href="#">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
+    <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
+    </svg>
+    </a>
 </body>
 </html>
